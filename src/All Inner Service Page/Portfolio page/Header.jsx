ee9fb8a96cbe5cd583cfa2f648fa57c13c1a4648
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-24 05:41:57 -0700

    Adjusting the Portfolio page

--- src/All Inner Service Page/Portfolio page/Header.jsx
+++ src/All Inner Service Page/Portfolio page/Header.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Wrapper>
+        <div className="container">
+            <h1>standard portfolio</h1>
+            <div className="tab">
+                <button>All Portfolio</button>
+                <button>fintech</button>
+                <button>education</button>
+                <button>SaaS & CRM </button>
+                <button>E-commerce</button>
+            </div>
+        </div>
+    </Wrapper>
   )
 }
 
