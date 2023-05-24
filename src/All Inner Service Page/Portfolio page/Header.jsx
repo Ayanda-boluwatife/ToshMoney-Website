0c9cb2b2d55d3705d4a4370c9e97fa76d7d0d3e3
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-24 05:47:21 -0700

    Adjusting the Portfolio page

--- src/All Inner Service Page/Portfolio page/Header.jsx
+++ src/All Inner Service Page/Portfolio page/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Header = () => {
   return (
@@ -16,5 +17,42 @@ const Header = () => {
     </Wrapper>
   )
 }
+const Wrapper = styled.div`
 
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .container{
+        width: 70%;
+        text-transform: capitalize;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+    .tab{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+    button{
+        padding: 12px 28px;
+        border: none;
+        color: black;
+        background-color: #F9F9F9;
+    }
+    button:hover, .active{
+        background-color:#FB9B02;
+    }
+
+`
 export default Header;
