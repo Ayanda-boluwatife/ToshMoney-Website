c62d2511c992f0bd83b03db1792e989268ddebdd
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-05-28 17:23:02 +0100

    updating the inner blog page

--- src/BlogPost/InnerBlog.jsx
+++ src/BlogPost/InnerBlog.jsx
@@ -1,53 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import Love from './Love'
 
 const InnerBlog = (props) => {
   return (
     <Wrapper>
-        <div className='innerblog__grid__1'>
-          <div>
-              <h2>{props.title}</h2>
+        <div className="grid__system">
+          <div className='innerblog__grid__1'>
+            <div>
+                <h2>{props.title}</h2>
+            </div>
+            <div className='innerblog__main'>
+                <img src={props.blogimg} alt="" />
+                <p>{props.para1}</p>
+                <p>{props.para2}</p>
+                <ol>
+                  <li>
+                    <h1>{props.para_header1}</h1>
+                    <p>{props.para3}</p>
+                  </li>
+                  <li>
+                    <h1>{props.para_header2}</h1>
+                    <p>{props.para4}</p>
+                  </li>
+                  <li>
+                    <h1>{props.para_header3}</h1>
+                    <p>{props.para5}</p>
+                  </li>
+                </ol>
+            </div>
           </div>
-          <div className='innerblog__main'>
-              <img src={props.blogimg} alt="" />
-              <p>{props.para1}</p>
-              <p>{props.para2}</p>
-              <ol>
-                <li>
-                  <h1>{props.para_header1}</h1>
-                  <p>{props.para3}</p>
-                </li>
-                <li>
-                  <h1>{props.para_header2}</h1>
-                  <p>{props.para4}</p>
-                </li>
-                <li>
-                  <h1>{props.para_header3}</h1>
-                  <p>{props.para5}</p>
-                </li>
-              </ol>
+
+          <div className='innerblog__grid__2'>
+            <div className='border__1'>
+              <h2>On this page</h2>
+              <ul>
+                <li>{props.para_header1}</li>
+                <li>{props.para_header2}</li>
+                <li>{props.para_header3}</li>
+              </ul>
+            </div>
+            <div className='border__2'>
+            <h2>Recommend blogs</h2>
+              <ul>
+                <li><a href="#">{props.list6}</a></li>
+                <li><a href="#">{props.list7}</a></li>
+                <li><a href="#">{props.list8}</a></li>
+                <li><a href="#">{props.list9}</a></li>
+                <li><a href="#">{props.list10}</a></li>
+              </ul>
+            </div>
           </div>
         </div>
 
-        <div className='innerblog__grid__2'>
-          <div className='border__1'>
-            <h2>On this page</h2>
-            <ul>
-              <li>{props.para_header1}</li>
-              <li>{props.para_header2}</li>
-              <li>{props.para_header3}</li>
-            </ul>
-          </div>
-          <div className='border__2'>
-          <h2>Recommend blogs</h2>
-            <ul>
-              <li><a href="#">{props.list6}</a></li>
-              <li><a href="#">{props.list7}</a></li>
-              <li><a href="#">{props.list8}</a></li>
-              <li><a href="#">{props.list9}</a></li>
-              <li><a href="#">{props.list10}</a></li>
-            </ul>
-          </div>
+        <div>
+          <Love />
         </div>
 
     </Wrapper>
@@ -55,11 +62,13 @@ const InnerBlog = (props) => {
 }
 const Wrapper = styled.section`
 
-padding: 0 4rem;
-display: grid;
-grid-template-columns: 75% 25%;
-grid-gap: 2rem;
-margin-bottom: 3rem;
+.grid__system{
+  padding: 0 4rem;
+  display: grid;
+  grid-template-columns: 75% 25%;
+  grid-gap: 2rem;
+  margin-bottom: 3rem;
+}
 
 .border__1, .border__2{
   border: 2px solid rgba(0, 0, 0, 0.1);
@@ -111,13 +120,16 @@ h1, h2, h3{
 }
 
 @media screen and (max-width:1024px) {
-
-  grid-template-columns: 1fr;
+  .grid__system{
+    grid-template-columns: 1fr;
+  }
 }
 
 @media screen and (max-width:600px) {
-  padding: 0 1rem;
-  grid-template-columns: 1fr;
+  .grid__system{
+    padding: 0 1rem;
+    grid-template-columns: 1fr;
+  }
 
   .innerblog__main img{
     height: 300px;
